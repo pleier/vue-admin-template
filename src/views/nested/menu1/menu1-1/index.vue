47a915410235e81d6904c787a76a0dab0ae312b8
plei <pleiyang@outlook.com>
2019-03-22 09:45:55 +0800

    Initial commit

---
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -0,0 +1,13 @@
+<template >
+  <div style="padding:30px;">
+    <el-alert :closable="false" title="menu 1-1" type="success">
+      <el-input type="text"/>
+      <router-view />
+    </el-alert>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Menu1'
+}
+</script>
---
+++ src/views/nested/menu2/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="padding:30px;">
+    <el-alert :closable="false" title="menu 2" />
+    <el-input type="text"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Menu2'
+}
+</script>
